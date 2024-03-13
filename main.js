fae3d95c541efc29f79cec053622da71787a3cda
bluestar_ <canikissfemboy@gmail.com>
2024-03-13 10:55:29 +0700

    Add translation for subtitle

--- main.js
+++ main.js
@@ -3,18 +3,17 @@ import createI18nModule from "./i18n"
 
 const locales = {
   en: {
-    "header": {
-      "title": "Frequently asked questions"
-    }
+    header: {
+      title: "Frequently asked questions",
+      subtitle: "Commonly asked questions about sumtum osu! Bancho.",
+    },
   },
   th: {
-    "header": {
-      "title": "คำถามที่พบบ่อย"
-    }
-    
-  }
-};
+    header: {
+      title: "คำถามที่พบบ่อย",
+      subtitle: "คำถามที่พบบ่อยเกี่ยวกับ somtum osu! bancho",
+    },
+  },
+}
 
-
-
-createI18nModule(locales);
+createI18nModule(locales)
